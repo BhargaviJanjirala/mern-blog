48b4f73163780f94d32069a1220ea4162121aeea
Janjirala Bhargavi <janjiralabhargavi9391@gmail.com>
2024-03-21 23:30:24 +0530

    created signup page ui

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,66 @@
+import { Button, Label, TextInput } from "flowbite-react";
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-xl text-white">
+              Bhargavi's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Welcome to Bhargavi's Blog – your gateway to a world of inspiration
+            and connection. As the creator behind the blog, I'm excited to share
+            my passion for storytelling and exploration with you. Here, you'll
+            find a diverse array of content curated with care, covering topics
+            ranging from technology and lifestyle to travel and personal
+            reflections.
+          </p>
+        </div>
+        {/*right*/}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Username" />
+              <TextInput
+                type="text"
+                placeholder="Enter your username"
+                id="username"
+              />
+            </div>
+            <div>
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="Enter your email"
+                id="email"
+              />
+            </div>
+            <div>
+              <Label value="Password" />
+              <TextInput
+                type="password"
+                placeholder="Enter your password"
+                id="password"
+              />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
