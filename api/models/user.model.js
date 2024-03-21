cb6b0cb0b573f6997c28a0745f034852826a3a18
Janjirala Bhargavi <janjiralabhargavi9391@gmail.com>
2024-03-21 15:49:11 +0530

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: Email,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          // Regular expression to validate email format
+          return /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/.test(v);
+        },
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
