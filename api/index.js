54e28d1dc2fff4c2ac3c77d660f387b47af252da
Janjirala Bhargavi <janjiralabhargavi9391@gmail.com>
2024-03-29 12:26:20 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 import commonRoutes from "./routes/comment.route.js";
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -13,6 +14,7 @@ mongoose
   .then(() => console.log("Mongo connected"))
   .catch((err) => console.log(err));
 
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +27,11 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commonRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
