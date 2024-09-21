d28c9b4a31f6fb27d84156ef1afa60574dbae7fc
Janjirala Bhargavi <janjiralabhargavi9391@gmail.com>
2024-09-21 17:35:58 +0530

    completed application

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -14,5 +14,5 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
-router.delete("/deleteuser", verifyToken, deleteUser);
+
 export default router;
